1e5cb29c2f24387082d3096fcfc72eb513bb186f
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-07-12 16:01:06 +0100

    Created the Root and understood it

--- src/main/java/com/enset/first_arena_game/HelloApplication.java
+++ src/main/java/com/enset/first_arena_game/HelloApplication.java
@@ -2,7 +2,9 @@ package com.enset.first_arena_game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +12,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+
+        Group root = new Group();
+        Region region = new Region();
+        //WebView webView = new WebView();
+        Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
+        stage.setTitle("Arena Game");
         stage.show();
     }
 
