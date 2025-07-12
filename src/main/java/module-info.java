3a83144f43862ded07ba17a491cf00109f47582f
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-07-12 15:04:59 +0100

    Second commit test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.enset.first_arena_game {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.enset.first_arena_game to javafx.fxml;
+    exports com.enset.first_arena_game;
+}
